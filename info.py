f35a91d0564c95a685d176b3aa0a808bc8c076bc
Himalaglebruhv27 <slavaseannextgeneration@gmail.com>
2024-09-26 19:42:30 +0800

    Update info.py

---
+++ info.py
@@ -0,0 +1,23 @@
+import sys,os
+from colorama import Fore
+print(Fore.MAGENTA+"""
+
+Backline-DDOS is bunch of tools which we made for people.These all tools are for purpose of ethical hacking and discord tools.
+
+
+""")
+print(Fore.RED+"""Getting Started
+
+git clone https://github.com/Himalagle-exc27/Backline-DDOS
+cd Backline-DDOS
+python -m pip install -r requirements.txt
+for(Windows user just run the bat file (start.bat) )
+Run Python backline.py
+
+
+License
+Backline-DDOS is under the MIT License
+Using it without giving us credit would lead to Breaking the License law
+
+
+""")
